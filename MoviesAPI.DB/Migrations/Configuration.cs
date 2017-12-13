5b1d1319a2dc544e5f7576f1550a213097953f20
Bartek <bartek.smarzeniec@gmail.com>
2017-12-13 17:23:16 +0100

    Seed overr

--- MoviesAPI.DB/Migrations/Configuration.cs
+++ MoviesAPI.DB/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using SwitchesAPI.DB.DbModels;
+
 namespace SwitchesAPI.DB.Migrations
 {
     using System;
@@ -18,6 +20,25 @@ namespace SwitchesAPI.DB.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            context.Rooms.AddOrUpdate(x => x.Id,
+                new Room() { Id = 1, Name = "Bedroom", Description = "Bedroom...", CreateDate = DateTime.Now },
+
+                new Room() { Id = 2, Name = "Kitchen", Description = "Kitchen...", CreateDate = DateTime.Now },
+
+                new Room() { Id = 3, Name = "Living Room", Description = "Living Room...", CreateDate = DateTime.Now }
+            );
+
+            context.Switches.AddOrUpdate(x => x.Id,
+                new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 1, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 1, Name = "Night lamp", Description = "Nigh lamp switch...", RoomId = 1, State = "OFF", CreateDate = DateTime.Now },
+
+                new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 1, Name = "Led strap", Description = "Led strap above cabinet...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
+
+                new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 1, Name = "Led strap", Description = "Led strap above cabinet...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now }
+            );
         }
     }
 }

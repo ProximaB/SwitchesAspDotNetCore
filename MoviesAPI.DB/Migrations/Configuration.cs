2b65d78166d5dce429ba8e347885e82310a00216
Bartek <bartek.smarzeniec@gmail.com>
2017-12-13 17:27:56 +0100

    Seed overr

--- MoviesAPI.DB/Migrations/Configuration.cs
+++ MoviesAPI.DB/Migrations/Configuration.cs
@@ -26,18 +26,23 @@ namespace SwitchesAPI.DB.Migrations
 
                 new Room() { Id = 2, Name = "Kitchen", Description = "Kitchen...", CreateDate = DateTime.Now },
 
-                new Room() { Id = 3, Name = "Living Room", Description = "Living Room...", CreateDate = DateTime.Now }
+                new Room() { Id = 3, Name = "Living Room", Description = "Living Room...", CreateDate = DateTime.Now },
+
+                new Room() { Id = 4, Name = "Bathroom", Description = "Bathroom..", CreateDate = DateTime.Now }
             );
 
             context.Switches.AddOrUpdate(x => x.Id,
                 new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 1, State = "OFF", CreateDate = DateTime.Now },
-                new Switch() { Id = 1, Name = "Night lamp", Description = "Nigh lamp switch...", RoomId = 1, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 2, Name = "Night lamp", Description = "Nigh lamp switch...", RoomId = 1, State = "OFF", CreateDate = DateTime.Now },
 
-                new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
-                new Switch() { Id = 1, Name = "Led strap", Description = "Led strap above cabinet...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 3, Name = "Main Light", Description = "Main light switch...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 4, Name = "Led strap", Description = "Led strap above cabinet...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
 
-                new Switch() { Id = 1, Name = "Main Light", Description = "Main light switch...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now },
-                new Switch() { Id = 1, Name = "Led strap", Description = "Led strap above cabinet...", RoomId = 2, State = "OFF", CreateDate = DateTime.Now }
+                new Switch() { Id = 5, Name = "Main Light", Description = "Main light switch...", RoomId = 3, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 6, Name = "Power outlet", Description = "Power outlet behind the tv...", RoomId = 3, State = "OFF", CreateDate = DateTime.Now },
+
+                new Switch() { Id = 7, Name = "Main Light", Description = "Main light switch...", RoomId = 4, State = "OFF", CreateDate = DateTime.Now },
+                new Switch() { Id = 8, Name = "Shower light", Description = "Shower ceiling light...", RoomId = 4, State = "OFF", CreateDate = DateTime.Now }
             );
         }
     }
